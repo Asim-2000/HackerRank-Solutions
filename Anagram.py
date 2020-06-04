1c661fc6d80d4f2e88270428b0a36cf8be519a1d
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-04 12:50:49 +0500

    Anagram.py completed

--- Anagram.py
+++ Anagram.py
@@ -1,3 +1,4 @@
+#Building a dictionary ot map to store the frequency count of characters in the substring 
 def buildMap(s):
     map = {}
     for ch in s:
@@ -10,21 +11,27 @@ def buildMap(s):
      
  
 def anagram(s):
+    #return -1 if the number is odd
     if len(s)%2 == 1:
         return -1
-         
+
+    #Taking the middle index     
     mid = len(s)//2
+    #first substring from index 0 to mid_index-1
     s1 = s[:mid]
+    #second substring from mid_index to last
     s2 = s[mid:]
-     
+    #building map for s1
     map_1 = buildMap(s1)
+    #building map for s2
     map_2 = buildMap(s2)
-     
+    
+    #counting the different letters
     diff_count = 0
     for key in map_2.keys():
         if key not in map_1:
-            diff_cnt += map_2[key]
+            diff_count += map_2[key]
         else:
-            diff_cnt += max(0, map_2[key]-map_1[key])
+            diff_count += max(0, map_2[key]-map_1[key])
      
     return diff_count
